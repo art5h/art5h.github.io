40d8eb7e061e0be3da9eb98bae4cc2530d013911
art5h <36566939+art5h@users.noreply.github.com>
2024-03-16 20:58:37 +0300

    Update Web

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// Assuming 'node' is the target element after which you want to insert the smiley
+var node = document.getElementById('head');
+
+// Function to generate a random number within a range
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Generate random position
+for (i = 0; i < 100; i++) {
+  var randomTop = getRandomNumber(0, window.innerHeight); // Adjust 50 to the height of your smiley if different
+  var randomLeft = getRandomNumber(0, window.innerWidth); // Adjust 50 to the width of your smiley if different
+
+  var emoji = ['🛡️', '⚔️', '💥', '🗡️', '🏹', '💣'];
+  var randomEmoji = getRandomNumber(0, emoji.length);
+  
+  var randomEmoji = emoji[randomEmoji];
+
+  node.insertAdjacentHTML('afterend', `<div class="smiley" style="position: absolute; top: ${randomTop}px; left: ${randomLeft}px;">${randomEmoji}</div>`);
+
+}
+
 let tg = window.Telegram.WebApp;
 
 tg.expand();
@@ -7,98 +29,7 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
-
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
-
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
-
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
-
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
-
-
-
-
-
-
-
-
+function start(){
+	tg.sendData(1);
+};
